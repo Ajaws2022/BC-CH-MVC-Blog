348924753d9e11d0c213ca8e7dbfe75529d1bd2f
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2023-02-19 15:41:35 -0800

    added login/signup js for use in handlebars

---
+++ public/js/login.js
@@ -0,0 +1,53 @@
+const Post = require("../../models/Post");
+
+const loginHandler = async (event) => {
+    event.preventDefault();
+
+    const userName = document.querySelector('#userName').value.trim();
+
+    const password = document.querySelector('#password').value.trim();
+
+    if (userName && password){
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({userName, password}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if(response.ok){
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+const signUpHandler = async (event) => {
+    event.preventDefault();
+
+    const userName = document.querySelector('#newUserName').value.trim();
+    const password = document.querySelector('#newPassword').value.trim();
+
+    if (userName && password){
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({userName, password}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if(response.ok){
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.signUpButton')
+    .addEventListener('click', signUpHandler);
+
+document
+    .querySelector('.loginButton')
+    .addEventListener('click', loginHandler);
+
